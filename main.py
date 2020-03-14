b808a4e2313652412d5f53eb61949301fdb1151c
danger3466 <danger3466@gmail.com>
2020-03-14 10:59:17 +0500

    init commit

---
+++ main.py
@@ -0,0 +1,29 @@
+class DialogMachine():
+    DIALOG = ['echo']
+    
+    def __init__(self):
+        self.__next_hop = 0
+
+    next_hop = property()
+    
+    @next_hop.getter
+    def next_hop(self):
+        return self.__next_hop
+
+    @next_hop.setter
+    def next_hop(self, value):
+        if isinstance(value, str):
+            value = self.DIALOG.index(value)
+
+        if value >= len(self.DIALOG):
+            self.__next_hop = 0
+        else:
+            self.__next_hop = value
+
+    def response(self, request):
+        function = getattr(self, self.DIALOG[self.next_hop])
+        return function(request)
+
+    def echo(self, request):
+        return request
+
